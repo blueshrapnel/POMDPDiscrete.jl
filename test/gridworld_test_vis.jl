0048810b77a795ab2ec49315dc075007154a4b4a
Karen Archer <laren.archer@gmail.com>
2022-04-12 11:59:57 +0100

    sample render output for agent location plot

--- test/gridworld_test_vis.jl
+++ test/gridworld_test_vis.jl
@@ -11,7 +11,8 @@ using Test
     mdp = GridWorld()
     rng = MersenneTwister(1234)
     s0 = rand(rng, initialstate(mdp))
+    # test plot of the gridworld and agent location 
     p = render(mdp, s=s0);
     @test p isa Plots.Plot
-    savefig(p, "test_render.png")
+    savefig(p, "test_render_agent_location.png")  
 end
