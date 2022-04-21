be12bff885f0d2406209159e9f1228b66d3fb9d6
Karen Archer <laren.archer@gmail.com>
2022-04-21 13:57:32 +0100

    added test for gridworld without terminal state

--- test/test_build_probabilistic_model.jl
+++ test/test_build_probabilistic_model.jl
@@ -7,7 +7,7 @@ import POMDPDiscrete.build_probabilistic_model
 
 using Test
 
-@testset "test probability model for 2x2 deterministic gridworld" begin
+@testset "test 2x2 deterministic gridworld - State(1,1) absorbing" begin
     mdp = GridWorld(
         size=(2,2),
         p_transition=1.0,
@@ -19,6 +19,37 @@ using Test
     @test P[:, :, 1] == [1 1 1 1;
                          0 0 0 0;
                          0 0 0 0;
+                         0 0 0 0]
+    # State(2,1) - bottom right
+    @test P[:, :, 2] == [0 0 0 1;
+                         0 1 1 0;
+                         0 0 0 0;
+                         1 0 0 0]
+    # State(1,2) - top left
+    @test P[:, :, 3] == [0 0 1 0;
+                         0 0 0 0;
+                         1 0 0 1;
+                         0 1 0 0]
+    # State(2,2) - top right
+    @test P[:, :, 4] == [0 0 0 0;
+                         0 0 1 0;
+                         0 0 0 1;
+                         1 1 0 0]
+
+end
+
+@testset "test 2x2 deterministic gridworld - no terminal state" begin
+    mdp = GridWorld(
+        size=(2,2),
+        p_transition=1.0,
+        absorbing_states=State[],
+        γ=1.0)
+
+    P = POMDPDiscrete.build_probabilistic_model(mdp)
+    # State(1,1) - absorbing so all actions remain in State(1,1)
+    @test P[:, :, 1] == [0 0 1 1;
+                         0 1 0 0;
+                         1 0 0 0;
                          0 0 0 0]
     # State(2,1) - bottom right
     @test P[:, :, 2] == [0 0 0 1;
