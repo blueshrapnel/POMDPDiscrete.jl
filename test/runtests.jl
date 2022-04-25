903251f8df039c09890a7e72320e68f0473a5502
Karen Archer <laren.archer@gmail.com>
2022-04-25 10:59:44 +0100

    reordered test sets for clarity

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using SafeTestsets
 
-@safetestset "gridworld tests" begin
-    include("test_gridworld_dynamics.jl")
+@safetestset "gridworld state spacee tests" begin
     include("test_gridworld_vis.jl")
-    include("test_gridworld_stochastic_policy.jl")
-    include("test_build_probabilistic_model.jl")
     include("test_gridworld_dynamic_prog.jl")
+end
+
+@safetestest "gridworld mdp tests" begin
+    include("test_build_probabilistic_model.jl")
+    include("test_gridworld_stochastic_policy.jl")
+    include("test_gridworld_dynamics.jl")
 end
